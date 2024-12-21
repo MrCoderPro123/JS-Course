873b9e2b8939d79102acac789295a2ba82c4c517
MrCoderPro123 <ahmedegamer23@gmail.com>
2024-12-21 20:22:16 +0500

    Learned Type Conversion

---
+++ typeConversion.js
@@ -0,0 +1,36 @@
+// let score = 69;
+
+// // Convert the score to a Number
+// let scoreAsString = Number(score);
+// console.log(typeof scoreAsString); // Output: string
+// console.log(scoreAsString); // Output: 69
+
+/*
+    69 => 69
+    "69" => 69
+    "69abc" => NaN
+    null => 0
+    undefined => NaN
+    true => 1
+    false => 0
+*/
+
+// Convert Number to boolean
+
+// let num = 0
+// let bool = Boolean(num);
+// console.log(bool); // Output: false
+// console.log(typeof bool); // Output: boolean
+
+
+/* 
+    0 => false
+    1 => true
+    "" (Empty String) => false
+    "abcd" (Non-empty String) => true
+    null => false
+    undefined => false
+*/
+
+
+console.log(Boolean(0)); // Output: false
