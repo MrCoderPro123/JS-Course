a00c3883dfe8a0100363b610aed6178982b2862b
MrCoderPro123 <ahmedegamer23@gmail.com>
2024-12-21 14:40:40 +0500

    Few more practice

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hello World");
+
+// const accId = 512689;
+// var accName = "John Doe";
+// let accBalance = 1000;
+// accLocation = "Karachi";
+
+// console.table([accId, accName, accBalance, accLocation]);
+
+// "use strict"; // By using this statement, we can only use newer version of JS
+
+
