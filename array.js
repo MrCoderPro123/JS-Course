1fd26447e9ebac5ebbe70b90a34c19ddc3cdf277
MrCoderPro123 <ahmedegamer23@gmail.com>
2024-12-22 14:33:22 +0500

    Learned about Arrays

---
+++ array.js
@@ -0,0 +1,20 @@
+// let myArr = ["Ironman", "Thor", "CAP"]
+
+// myArr.push("Hulk")
+// myArr.pop()
+// let newArr = myArr.splice(1, 2)
+
+// console.log(newArr);
+// console.log(myArr);
+
+// let heros = ["Ertugrul", "Bamsi", "Turgut", "Halima"]
+// let villains = ["Noyan", "Kurtoglu", "Titus", "Ural"]
+
+// let allCharacters = heros.concat(villains)
+// let allCharacters = [...heros, ...villains]
+
+// console.log(Array.from("Ertugrul"));
+// console.log(Array.of("Ertugrul", "Noyan"));
+
+
+// console.log(allCharacters);
